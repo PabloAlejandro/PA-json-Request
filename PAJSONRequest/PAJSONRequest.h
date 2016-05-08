778bf293a397aba818ec5d59de2b64dd0f6a525b
Pablo A. Pérez-Martínez <pablo.alejandro.pm@gmail.com>
2016-05-08 20:11:04 +0200

    Files added

--- PAJSONRequest/PAJSONRequest.h
+++ PAJSONRequest/PAJSONRequest.h
@@ -15,5 +15,5 @@ FOUNDATION_EXPORT double PAJSONRequestVersionNumber;
 FOUNDATION_EXPORT const unsigned char PAJSONRequestVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <PAJSONRequest/PublicHeader.h>
-
-
+#import <PAJSONRequest/Utils/Network/JSONServerRequest/JSONRequestRetry.h>
+#import <PAJSONRequest/Utils/Network/JSONServerRequest/JSONRequestsManager.h>

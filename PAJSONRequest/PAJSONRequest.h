fba9b8fbb920c4ce8d66936a2a94d546dd81e85b
Pablo A. Pérez-Martínez <pablo.alejandro.pm@gmail.com>
2016-05-08 20:04:29 +0200

    First commit

---
+++ PAJSONRequest/PAJSONRequest.h
@@ -0,0 +1,19 @@
+//
+//  PAJSONRequest.h
+//  PAJSONRequest
+//
+//  Created by Pau on 08/05/2016.
+//  Copyright © 2016 Pablo Alejandro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PAJSONRequest.
+FOUNDATION_EXPORT double PAJSONRequestVersionNumber;
+
+//! Project version string for PAJSONRequest.
+FOUNDATION_EXPORT const unsigned char PAJSONRequestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PAJSONRequest/PublicHeader.h>
+
+
